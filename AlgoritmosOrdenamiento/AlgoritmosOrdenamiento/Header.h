7d5b1a85794402177e8bf639b1e1de40d33856b2
Angel Loayza <angelloayzah2016@gmail.com>
2022-04-06 01:29:38 -0500

    Implementacion de 3 nuevos algorimos, junto a un header.h de los algoritmos originales

---
+++ AlgoritmosOrdenamiento/AlgoritmosOrdenamiento/Header.h
@@ -0,0 +1,284 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int A[], int size)
+{
+    cout << "----------------------------------------------" << endl;
+    for (auto i = 0; i < size; i++)
+        cout << A[i] << " ";
+}
+
+//bouble
+
+void booble()
+{
+    int temp;
+    int a[10] = { 20,54,14,2,4,7,8,32,85,1 };
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = i + 1; j < 10; j++)
+        {
+            if (a[j] < a[i]) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    cout << "lista ordenada" << endl;
+
+    for (int i = 0; i < 10; i++) {
+        cout << a[i] << "\t";
+    }
+}
+
+//insert
+void insert()
+{
+    int a[10] = { 20,54,14,2,4,7,8,32,85,1 };
+
+    for (int i = 1; i < 10; i++)
+    {
+        int temp = a[i];
+        int j = i - 1;
+        while (j >= 0 && temp <= a[j])
+        {
+            a[j + 1] = a[j];
+            j = j - 1;
+        }
+        a[j + 1] = temp;
+    }
+    cout << "lista ordenada" << endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << a[i] << "\t";
+    }
+}
+
+//select
+void selection()
+{
+    int a[10] = { 20,54,14,2,4,7,8,32,85,1 };
+    int i, j, n, loc, temp, min;
+
+    n = 10;
+
+    for (i = 0; i < n - 1; i++)
+    {
+        min = a[i];
+        loc = i;
+
+        for (j = i + 1; j < n; j++)
+        {
+            if (min > a[j])
+            {
+                min = a[j];
+                loc = j;
+            }
+        }
+
+        temp = a[i];
+        a[i] = a[loc];
+        a[loc] = temp;
+    }
+
+    cout << "lista ordenada" << endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << a[i] << "\t";
+    }
+}
+
+//cocktail
+void cocktail()
+{
+    int a[] = { 5,1,4,8,2,1,6,5,7 };
+    int n = sizeof(a) / sizeof(a[0]);
+
+    bool swapped = true;
+    int start = 0;
+    int end = n - 1;
+
+    while (swapped) {
+        swapped = false;
+
+        for (int i = start; i < end; ++i) {
+            if (a[i] > a[i + 1]) {
+                swap(a[i], a[i + 1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped)
+            break;
+
+        swapped = false;
+        --end;
+        for (int i = end - 1; i >= start; --i) {
+            if (a[i] > a[i + 1]) {
+                swap(a[i], a[i + 1]);
+                swapped = true;
+            }
+        }
+        ++start;
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+}
+
+//merge
+void mergeaux(int array[], int const left, int const mid, int const right)
+{
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+
+    // Create temp arrays
+    auto* leftArray = new int[subArrayOne],
+        * rightArray = new int[subArrayTwo];
+
+    // Copy data to temp arrays leftArray[] and rightArray[]
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+
+    auto indexOfSubArrayOne = 0, // Initial index of first sub-array
+        indexOfSubArrayTwo = 0; // Initial index of second sub-array
+    int indexOfMergedArray = left; // Initial index of merged array
+    // Merge the temp arrays back into array[left..right]
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // left[], if there are any
+    while (indexOfSubArrayOne < subArrayOne) {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+
+void merge(int array[], int const begin, int const end)
+{
+    if (begin >= end)
+        return; // Returns recursively
+
+    auto mid = begin + (end - begin) / 2;
+    merge(array, begin, mid);
+    merge(array, mid + 1, end);
+    mergeaux(array, begin, mid, end);
+}
+
+//quick
+int partition(int arr[], int start, int end)
+{
+
+    int pivot = arr[start];
+
+    int count = 0;
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i] <= pivot)
+            count++;
+    }
+
+    // Giving pivot element its correct position
+    int pivotIndex = start + count;
+    swap(arr[pivotIndex], arr[start]);
+
+    // Sorting left and right parts of the pivot element
+    int i = start, j = end;
+
+    while (i < pivotIndex && j > pivotIndex) {
+
+        while (arr[i] <= pivot) {
+            i++;
+        }
+
+        while (arr[j] > pivot) {
+            j--;
+        }
+
+        if (i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
+    }
+
+    return pivotIndex;
+}
+
+void quick(int arr[], int start, int end)
+{
+
+    // base case
+    if (start >= end)
+        return;
+
+    // partitioning the array
+    int p = partition(arr, start, end);
+
+    // Sorting the left part
+    quick(arr, start, p - 1);
+
+    // Sorting the right part
+    quick(arr, p + 1, end);
+}
+
+//heap
+void heapify(int arr[], int n, int i)
+{
+    int largest = i; // Initialize largest as root
+    int l = 2 * i + 1; // left = 2*i + 1
+    int r = 2 * i + 2; // right = 2*i + 2
+
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    // If largest is not root
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+// main function to do heap sort
+void heapSort(int arr[], int n)
+{
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // One by one extract an element from heap
+    for (int i = n - 1; i >= 0; i--) {
+        // Move current root to end
+        swap(arr[0], arr[i]);
+
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
